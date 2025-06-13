9df8a638a8d2b3d69e4a7e34142b86d24f85b708
DaisukeKogure <kogure.d.816@ms.saitama-u.ac.jp>
2025-06-13 15:40:46 +0900

    create_rk4

--- src/particle.jl
+++ src/particle.jl
@@ -24,6 +24,39 @@ function timeprop(F::Function, tmax::Float64, x_0::Float64, a_0::Float64, h::Flo
     return x,a
 end
 
+# Runge-Kutta 4th order method for time propagation
+function rk4prop(F::Function, tmax::Float64, x0::Float64, v0::Float64, h::Float64)
+    t = 0.0
+    x = x0
+    v = v0
+    numsteps = Int(floor(tmax / h))
+
+    for i in 1:numsteps
+        # k1
+        dx1 = v
+        dv1 = F(x, t)
+
+        # k2
+        dx2 = v + 0.5 * h * dv1
+        dv2 = F(x + 0.5 * h * dx1, t + 0.5 * h)
+
+        # k3
+        dx3 = v + 0.5 * h * dv2
+        dv3 = F(x + 0.5 * h * dx2, t + 0.5 * h)
+
+        # k4
+        dx4 = v + h * dv3
+        dv4 = F(x + h * dx3, t + h)
+
+        # update x, v
+        x += h * (dx1 + 2dx2 + 2dx3 + dx4) / 6
+        v += h * (dv1 + 2dv2 + 2dv3 + dv4) / 6
+        t += h
+    end
+
+    return x, v
+end
+
 
 # function timeprop_picture(F::Function, tmax::Float64, x_0::Float64, a_0::Float64, h::Float64)::Tuple{Vector{Float64}, Vector{Float64}}
 
